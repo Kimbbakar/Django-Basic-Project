a086308ed050b7db85f6f70e5e8727679cec338c
Kimbbakar <chowdhuryosman04@gmail.com>
2017-10-02 22:46:41 +0600

    added data base with django

--- my_django17_project/my_django17_project/settings.py
+++ my_django17_project/my_django17_project/settings.py
@@ -72,11 +72,13 @@ WSGI_APPLICATION = 'my_django17_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+ 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': '1234',
     }
 }
 
