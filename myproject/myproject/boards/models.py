a4510be799458013baa23d64a4ed7b5e88f551ac
Kimbbakar <chowdhuryosman04@gmail.com>
2017-10-21 23:01:06 +0600

    fix the indentation

--- myproject/myproject/boards/models.py
+++ myproject/myproject/boards/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Board(models.Model):
-	name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
 
 
