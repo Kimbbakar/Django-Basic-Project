54dd9ff4ec6934d6cd729b408e8d117e60b9c66f
Kimbbakar <chowdhuryosman04@gmail.com>
2017-10-30 23:44:55 +0600

    add url and html for a board topic

--- myproject/myproject/boards/views.py
+++ myproject/myproject/boards/views.py
@@ -6,13 +6,18 @@ from .models import Board
 def home(request):
 	boards = Board.objects.all()
 
-	boards_names  = list()
+	boards_names  = list()	
 
 	for board in boards:
 		boards_names.append(board.name)
-
 
 	response_html = '<br>'.join(boards_names)
 
 	return render(request,'home.html',{'boards':boards } )
 	return HttpResponse(response_html)
+
+
+def board_topics(request, pk):
+    board = Board.objects.get(pk=pk)
+    print (board)
+    return render(request, 'topics.html', {'board': board})	
--- myproject/myproject/myproject/urls.py
+++ myproject/myproject/myproject/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from boards import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^$', views.home,name='home' ),
+	url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^admin/', admin.site.urls),
 ]
