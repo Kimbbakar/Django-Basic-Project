e558aa6c23d0543a3a55517dda69d48dc941782e
Kimbbakar <chowdhuryosman04@gmail.com>
2017-11-02 20:49:11 +0600

    update: board/view

--- myproject/myproject/boards/views.py
+++ myproject/myproject/boards/views.py
@@ -6,18 +6,11 @@ from .models import Board
 def home(request):
 	boards = Board.objects.all()
 
-	boards_names  = list()	
+	boards_names  = list()	 
 
-	for board in boards:
-		boards_names.append(board.name)
-
-	response_html = '<br>'.join(boards_names)
-
-	return render(request,'home.html',{'boards':boards } )
-	return HttpResponse(response_html)
+	return render(request,'home.html',{'boards':boards } ) 
 
 
 def board_topics(request, pk):
-    board = Board.objects.get(pk=pk)
-    print (board)
+    board = Board.objects.get(pk=pk) 
     return render(request, 'topics.html', {'board': board})	
