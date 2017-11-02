ff54158a80d2a62a1bec93c6d43b224ba11660d9
Kimbbakar <chowdhuryosman04@gmail.com>
2017-11-03 02:39:29 +0600

    create new_topic

--- myproject/myproject/boards/views.py
+++ myproject/myproject/boards/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Board
+
 # Create your views here.
 
 def home(request):
@@ -14,3 +16,8 @@ def home(request):
 def board_topics(request, pk):
     board = Board.objects.get(pk=pk) 
     return render(request, 'topics.html', {'board': board})	
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'new_topic.html', {'board': board})
